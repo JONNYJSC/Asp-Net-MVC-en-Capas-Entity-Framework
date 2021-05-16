c86c21d9c87bc79c963803c2ca3affc6618dd108
JONNYJSC <jjonath21@gmail.com>
2021-05-16 13:59:47 -0600

    DataTables and Scripts, Css

---
+++ Web_Proyecto/Scripts/app.js
@@ -0,0 +1,21 @@
+﻿function ValidarFechas(dateIni, dateFin) {
+    var _dateIni = new Date(dateIni);
+    var _dateFin = new Date(dateFin);
+    if (_dateFin < _dateIni)
+        return false;
+    else
+        return true;
+}
+
+function LoadingOverlayShow(id) {
+    $(id).LoadingOverlay("Show", {
+        color: "rgba(255, 255, 255, 0.5)",
+        image: "/Content/loading.gif",
+        imageResizeFactor: 0.6,
+        //imageAnimation: "1.5s fadein",
+    });
+}
+
+function LoadingOverlayHide(id) {
+    $(id).LoadingOverlay("hide");
+}
